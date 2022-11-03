d8c04e0182c9b49d8fd558fb4a94e0e5de30ba23
maiwad <117359322+maiwad@users.noreply.github.com>
2022-11-03 06:09:14 -0700

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -11,19 +11,19 @@ if [[ ${xy_tagename} == ${xy_current} ]]
 then
     echo 'xy is nothing to do'
 else
-	git clone https://github.com/XTLS/Xray-core.git
-	cd Xray-core && go mod download
-	CGO_ENABLED=0 go build -o xy -trimpath -ldflags "-s -w -buildid=" ./main
-        mv xy ../releases/xy
-	cd ../
-	rm -rf Xray-core
+    git clone https://github.com/XTLS/Xray-core.git
+    cd Xray-core && go mod download
+    CGO_ENABLED=0 go build -o xy -trimpath -ldflags "-s -w -buildid=" ./main
+    mv xy ../releases/xy
+    cd ../
+    rm -rf Xray-core
 fi
 if [[ ${caddy_latest} == ${caddy_current} ]]
 then
     echo 'cddy is nothing to do'
 else
-	go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
-	xcaddy build latest --output ./releases/caddy
+    go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+    xcaddy build latest --output ./releases/caddy
 fi
 git config user.name github-actions
 git config user.email github-actions@github.com
